7e54848fe86e4b3ad7c864edc0388bc8a7f57eac
Martyn Whitwell <martyn.whitwell@gmail.com>
2017-05-25 17:47:40 +0100

    Further productionisation of geoblacklight

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-echo "Creating tmp and log, clearing out PIDs"
-mkdir -p $APP_HOME/tmp/pids $APP_HOME/log
-rm  -f $APP_HOME/tmp/pids/*
+echo "Creating log folder"
+mkdir -p $APP_WORKDIR/log
 
-# Install any missing gems
-bundle check || bundle install
+
+if [ "$RAILS_ENV" = "production" ]; then
+    # Verify all the production gems are installed
+    bundle check
+else
+    # install any missing development gems (as we can tweak the development container without rebuilding it)
+    bundle check || bundle install --without production
+fi
+
 
 # Run any pending migrations
 bundle exec rake db:migrate
 
-if [ "$RAILS_ENV" = "production" ]; then
-    echo "Compiling assets..."
-    bundle exec rake assets:clean assets:precompile
+if [ "$GEOBLACKLIGHT_SEED" = "true" ] ; then
+    echo "Seeding Solr with some data"
+    bundle exec rake geoblacklight:solr:seed
 fi
 
-echo "Seeding Solr with some data"
-bundle exec rake geoblacklight:solr:seed
 
 echo "Starting Geoblacklight"
-bundle exec rails s -p 3010 -b '0.0.0.0'
+rm -f /tmp/geoblacklight.pid
+bundle exec rails s -p 3010 -b '0.0.0.0' --pid /tmp/geoblacklight.pid
 
